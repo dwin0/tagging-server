b83097de319f47f88f359efb6fecfbef475a0578
aha93 <a1hauser@hsr.ch>
2017-04-08 16:28:49 +0200

    Erweitert um Geschwindigkeitsberechnung
    erste einfache Geschwindigkeitsberechnung

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,9 @@ const OsmQuery = 'WITH closest_candidates AS (SELECT id, osm_id, osm_name, clazz
     'SELECT id, osm_id, osm_name, clazz, ST_Distance(geom_way::geography, ST_GeomFromText(\'POINT({lon} {lat})\', 4326)::geography) FROM closest_candidates ' +
     'ORDER BY ST_Distance(geom_way, ST_GeomFromText(\'POINT({lon} {lat})\', 4326)) LIMIT 3;';
 
+const OsmQueryDistance = 'SELECT ST_Distance(ST_GeomFromText(\'POINT({lon1} {lat1})\',4326)::geography, ' +
+    'ST_GeomFromText(\'POINT({lon2} {lat2})\', 4326)::geography);';
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tagging-Prototype 1.0' });
 });
@@ -19,6 +22,61 @@ router.get('/', function(req, res, next) {
 String.prototype.replaceAll = function(target, replacement) {
     return this.split(target).join(replacement);
 };
+
+router.get('/speedCalculation', function (reg, res, next) {
+    res.render('speedIndex', { title: 'Geschwindigkeitsberechnung' });
+});
+
+router.get('/getSpeedCalculation', function(req, res, next) {
+    var lat1 = req.query.latitude1;
+    var lat2 = req.query.latitude2;
+
+    var lon1 = req.query.longitude1;
+    var lon2 = req.query.longitude2;
+
+    var startDate = new Date(req.query.start);
+    var endDate = new Date(req.query.end);
+
+    var resultingDistance;
+    var resultingTime;
+    var resultingVelocityMS;
+    var resultingVelocityKMH;
+
+    pg.connect(connectionString, function (err, client, done) {
+        if (err) {
+            return console.error('error fetching client from pool', err)
+        }
+        done();
+
+        var queryStatement = OsmQueryDistance
+            .replaceAll("{lon1}", lon1)
+            .replaceAll("{lat1}", lat1)
+            .replaceAll("{lon2}", lon2)
+            .replaceAll("{lat2}", lat2);
+
+        client.query(queryStatement, function (err, result) {
+            done();
+
+            if (err) {
+                return console.error('error happened during query', err)
+            }
+
+            resultingDistance = result.rows[0].st_distance;
+            resultingTime = (endDate - startDate) / 1000;
+            resultingVelocityMS = resultingDistance / resultingTime;
+            resultingVelocityKMH = (resultingDistance / 1000) / (resultingTime / 3600);
+
+            res.render('speedView', {
+                title: "Calculated Distance:",
+                distance: resultingDistance,
+                time: resultingTime,
+                velocity_ms: resultingVelocityMS,
+                velocity_kmh: resultingVelocityKMH
+            });
+        });
+    });
+
+});
 
 router.get('/getNearest', function (req, res) {
 
