35ebd4fae2deaef3698aaccb26fca1492de38da9
aha93 <a1hauser@hsr.ch>
2017-05-07 17:05:51 +0200

    population density und community type werden zurückgegeben

--- business_logic/v3/surroundings.js
+++ business_logic/v3/surroundings.js
@@ -1,8 +1,11 @@
 var db_access= require('../../persistence/db_access_v3');
 var helper = require('./helper');
 var parallel = require("async/parallel");
+var converter = require('./wgs84_ch1903');
+var request = require('request');
 
 
+//Constants for geographical surroundings:
 const GRASSLAND = {
     id: 1,
     name: "grassland",
@@ -52,6 +55,62 @@ const UNKNOWN = {
 };
 
 
+//Constants for community types:
+const LARGECENTRE = {
+    id: 1,
+    name: 'Grosszentrum',
+    description: 'Tag is derived from: Gemeindetypologie ARE (Bundesamt für Raumentwicklung)'
+};
+
+const BESIDESCENTREOFLARGECENTRE = {
+    id: 2,
+    name: 'Nebenzentrum eines Grosszentrums',
+    description: 'Tag is derived from: Gemeindetypologie ARE (Bundesamt für Raumentwicklung)'
+};
+
+const BELTOFLARGECENTRE = {
+    id: 3,
+    name: 'Gürtel eines Grosszentrums',
+    description: 'Tag is derived from: Gemeindetypologie ARE (Bundesamt für Raumentwicklung)'
+};
+
+const MEDIUMCENTRE = {
+    id: 4,
+    name: 'Mittelzentrum',
+    description: 'Tag is derived from: Gemeindetypologie ARE (Bundesamt für Raumentwicklung)'
+};
+
+const BELTOFMEDIUMCENTRE = {
+    id: 5,
+    name: 'Gürtel eines Mittelzentrums',
+    description: 'Tag is derived from: Gemeindetypologie ARE (Bundesamt für Raumentwicklung)'
+};
+
+const SMALLCENTRE = {
+    id: 6,
+    name: 'Kleinzentrum',
+    description: 'Tag is derived from: Gemeindetypologie ARE (Bundesamt für Raumentwicklung)'
+};
+
+const PERIURBAN = {
+    id: 7,
+    name: 'Periurbane ländliche Gemeinde',
+    description: 'Tag is derived from: Gemeindetypologie ARE (Bundesamt für Raumentwicklung)'
+};
+
+const AGRICULTURAL = {
+    id: 8,
+    name: 'Agrargemeinde',
+    description: 'Tag is derived from: Gemeindetypologie ARE (Bundesamt für Raumentwicklung)'
+};
+
+const TOURISTICAL = {
+    id: 9,
+    name: 'Touristische Gemeinde',
+    description: 'Tag is derived from: Gemeindetypologie ARE (Bundesamt für Raumentwicklung)'
+};
+
+
 const FIND_MIDDLE_POINT = "SELECT ST_AsText(ST_Centroid(ST_GeomFromText(('MULTIPOINT ( {lon1} {lat1}, {lon2} {lat2})'), 4326)));";
 
 const NATURAL_QUERY = 'WITH natural_match AS (SELECT "natural", wkb_geometry FROM multipolygons WHERE "natural" IS NOT NULL) ' +
@@ -66,6 +125,9 @@ const LEISURE_QUERY = 'WITH leisure_match AS (SELECT leisure, wkb_geometry FROM 
     'SELECT leisure FROM leisure_match ' +
     'WHERE ST_Within(ST_GeomFromText((\'{point}\'), 4326), ST_GeomFromEWKB(leisure_match.wkb_geometry))';
 
+const GEOADMIN_URL = 'https://api3.geo.admin.ch/rest/services/all/MapServer/identify?geometry={y},{x}' +
+    '&geometryFormat=geojson&geometryType=esriGeometryPoint&imageDisplay=1,1,1&lang=de&layers=all:ch.are.bevoelkerungsdichte,' +
+    'ch.are.gemeindetypen&mapExtent=0,0,1,1&returnGeometry=false&tolerance=5';
 
 function getGeographicalSurroundings(positions, callback) {
     var middlePointQueries = prepareMiddlePointDbStatements(FIND_MIDDLE_POINT, positions);
@@ -81,9 +143,9 @@ function getGeographicalSurroundings(positions, callback) {
             var naturalQueries = prepareNaturalDbStatements(NATURAL_QUERY, results);
             var boundaryQueries = prepareBoundaryAndLeisureDbStatements(BOUNDARY_QUERY, results);
             var leisureQueries = prepareBoundaryAndLeisureDbStatements(LEISURE_QUERY, results);
-            console.log(naturalQueries);
-            console.log(boundaryQueries);
-            console.log(leisureQueries);
+            //console.log(naturalQueries);
+            //console.log(boundaryQueries);
+            //console.log(leisureQueries);
 
             parallel([
                     function(callback) {
@@ -104,27 +166,164 @@ function getGeographicalSurroundings(positions, callback) {
                 ],
                 function (err, results) {
                     results[0].forEach(function (element) {
-                        console.log(element);
-                        console.log(element.length > 0);
+                        //console.log(element);
+                        //console.log(element.length > 0);
                     });
 
                     results[1].forEach(function (element) {
-                        console.log(element);
-                        console.log(element.length > 0);
+                        //console.log(element);
+                        //console.log(element.length > 0);
                     });
 
                     results[2].forEach(function (element) {
-                        console.log(element);
-                        console.log(element.length > 0);
+                        //console.log(element);
+                        //console.log(element.length > 0);
                     });
 
-                    returnTag(UNKNOWN, callback);
+                    returnSurroundingsTag(UNKNOWN, callback);
                 }
             );
         }
     );
 }
 
+function getGeoAdminData(positions, callback) {
+    var middlePointQueries = prepareMiddlePointDbStatements(FIND_MIDDLE_POINT, positions);
+
+    parallel([
+            function(callback) {
+                db_access.queryMultiple(db_access.getDatabase(db_access.SWITZERLAND_DB), middlePointQueries, function (result) {
+                    callback(null, result);
+                });
+            }
+        ],
+        function (err, results) {
+            var pointDownload = results[0][0][0].st_astext;
+            var pointUpload = results[0][1][0].st_astext;
+            var lonDownload = pointDownload.substring(6, pointDownload.indexOf(' '));
+            var latDownload = pointDownload.substring(pointDownload.indexOf(' ') + 1, pointDownload.length - 1);
+            var lonUpload = pointUpload.substring(6, pointUpload.indexOf(' '));
+            var latUpload = pointUpload.substring(pointUpload.indexOf(' ') + 1, pointUpload.length - 1);
+            var chyDownload = converter.WGStoCHy(latDownload, lonDownload);
+            var chxDownload = converter.WGStoCHx(latDownload, lonDownload);
+            var chyUpload = converter.WGStoCHy(latUpload, lonUpload);
+            var chxUpload = converter.WGStoCHx(latUpload, lonUpload);
+            var urlDownload = GEOADMIN_URL.replace('{y}', chyDownload).replace('{x}', chxDownload);
+            var urlUpload = GEOADMIN_URL.replace('{y}', chyUpload).replace('{x}', chxUpload);
+
+            parallel([
+                    function(callback) {
+                        request.get(
+                            urlDownload,
+                            function (error, response) {
+                                if (!error && response.statusCode === 200) {
+                                    callback(null, JSON.parse(response.body));
+                                } else {
+                                    console.error("error");
+                                }
+                            }
+                        );
+                    },
+                    function(callback) {
+                        request.get(
+                            urlUpload,
+                            function (error, response) {
+                                if (!error && response.statusCode === 200) {
+                                    callback(null, JSON.parse(response.body));
+                                } else {
+                                    console.error("error");
+                                }
+                            }
+                        );
+                    }
+                ],
+                function (err, results) {
+                    var downloadResult = results[0];
+                    var uploadResult = results[1];
+
+                    var populationDensityDownload;
+                    var populationDensityDownloadTotal = 0;
+                    var communityTagDownload = UNKNOWN;
+                    var communityTypeDownload = { comId: -1, comName: 'unknown', canId: -1, canName: 'unknown' };
+
+                    var populationDensityUpload;
+                    var populationDensityUploadTotal = 0;
+                    var communityTagUpload = UNKNOWN;
+                    var communityTypeUpload = { comId: -1, comName: 'unknown', canId: -1, canName: 'unknown' };
+
+                    var i = 0;
+                    var j = 0;
+
+                    downloadResult.results.forEach(function (element) {
+                        if(element.layerBodId === 'ch.are.bevoelkerungsdichte') {
+                            i++;
+                            populationDensityDownloadTotal += element.properties.popt_ha;
+                        } else if(element.layerBodId === 'ch.are.gemeindetypen') {
+                            communityTagDownload = getTag(element.properties.typ_code);
+                            communityTypeDownload.canName = element.properties.kt_kz;
+                            communityTypeDownload.canId = element.properties.kt_no;
+                            communityTypeDownload.comName = element.properties.label;
+                            communityTypeDownload.comId = element.properties.bfs_no;
+                        }
+                    });
+
+                    populationDensityDownload = i > 0 ? populationDensityDownloadTotal / i : populationDensityDownloadTotal;
+
+                    uploadResult.results.forEach(function (element) {
+                        if(element.layerBodId === 'ch.are.bevoelkerungsdichte') {
+                            j++;
+                            populationDensityUploadTotal += element.properties.popt_ha;
+                        } else if(element.layerBodId === 'ch.are.gemeindetypen') {
+                            communityTagUpload = getTag(element.properties.typ_code);
+                            communityTypeUpload.canName = element.properties.kt_kz;
+                            communityTypeUpload.canId = element.properties.kt_no;
+                            communityTypeUpload.comName = element.properties.label;
+                            communityTypeUpload.comId = element.properties.bfs_no;
+                        }
+                    });
+
+                    populationDensityUpload = j > 0 ? populationDensityUploadTotal / i : populationDensityUploadTotal;
+
+                    var resultingTags = { download: {
+                        pop:
+                            { number: populationDensityDownload, description: 'Number of people living in 1ha' },
+                        type: { tag: communityTagDownload, res: communityTypeDownload }
+                    }, upload: {
+                        pop:
+                            { number: populationDensityUpload, description: 'Number of people living in 1ha' },
+                        type: { tag: communityTagUpload, res: communityTypeUpload }
+                    }};
+                    callback(resultingTags);
+                }
+            );
+        }
+    );
+}
+
+function getTag(number) {
+    switch(number) {
+        case "1":
+            return LARGECENTRE;
+        case "2":
+            return BESIDESCENTREOFLARGECENTRE;
+        case "3":
+            return BELTOFLARGECENTRE;
+        case "4":
+            return MEDIUMCENTRE;
+        case "5":
+            return BELTOFMEDIUMCENTRE;
+        case "6":
+            return SMALLCENTRE;
+        case "7":
+            return PERIURBAN;
+        case "8":
+            return AGRICULTURAL;
+        case "9":
+            return TOURISTICAL;
+        default:
+            return UNKNOWN;
+    }
+}
 
 function prepareMiddlePointDbStatements(statement, positions) {
     var statements = [];
@@ -167,9 +366,9 @@ function prepareBoundaryAndLeisureDbStatements(statement, points) {
 }
 
 
-function returnTag(tags, callback) {
-    var result = { download: { tag: tags, osm_tag: 'unknown' }, upload: { tag: tags, osm_tag: 'unknown2' } };
+function returnSurroundingsTag(tags, callback) {
+    var result = { download: { geo: tags, osm_tag: 'unknown' }, upload: { geo: tags, osm_tag: 'unknown2' } };
     callback(result);
 }
 
-module.exports = { "getGeographicalSurroundings": getGeographicalSurroundings };
+module.exports = { "getGeographicalSurroundings": getGeographicalSurroundings, "getGeoAdminData": getGeoAdminData };
--- business_logic/v3/surroundings_communication.js
+++ business_logic/v3/surroundings_communication.js
@@ -11,78 +11,91 @@ function getSurroundingsJSON(req, res) {
 
     positions = filterPositions(positions);
 
+    if(positions.length < 3) {
+        res.writeHead(400, {"Content-Type": "application/json"});
+        var json = JSON.stringify({
+            statusText: 'Bad Request',
+            description: 'Phases DownloadStart, DownloadEnd and UploadEnd where expected. At least one phase is missing.',
+            receivedElements: positions
+        });
+        res.end(json);
+    } else {
+        parallel([
+                function(callback) {
+                    surroundings.getGeographicalSurroundings(positions, function (result) {
+                        callback(null, result);
+                    });
+                },
+                function(callback) {
+                    surroundings.getGeoAdminData(positions, function (result) {
+                        callback(null, result);
+                    })
+                }
+            ],
+            function(err, results) {
+                //console.log(results);
+                var geographicalSurroundingsResult = results[0];
+                var geoAdminResults = results[1];
+                res.writeHead(200, {"Content-Type": "application/json"});
 
-    parallel([
-            function(callback) {
-                surroundings.getGeographicalSurroundings(positions, function (result) {
-                    callback(null, result);
-                });
-            }
-        ],
-        function(err, results) {
-            console.log(results);
-            var geographicalSurroundingsResult = results[0];
-            res.writeHead(200, {"Content-Type": "application/json"});
-
-            var json = JSON.stringify({
-                title: "Calculated Surroundings",
-                surroundings: {
-                    download: {
-                        geographical_surroundings: {
-                            id: geographicalSurroundingsResult.download.tag.id,
-                            name: geographicalSurroundingsResult.download.tag.name,
-                            osm_tag: geographicalSurroundingsResult.download.osm_tag,
-                            description: geographicalSurroundingsResult.download.tag.description,
-                            probability: null
+                var json = JSON.stringify({
+                    title: "Calculated Surroundings",
+                    surroundings: {
+                        download: {
+                            geographical_surroundings: {
+                                id: geographicalSurroundingsResult.download.geo.id,
+                                name: geographicalSurroundingsResult.download.geo.name,
+                                osm_tag: geographicalSurroundingsResult.download.osm_tag,
+                                description: geographicalSurroundingsResult.download.geo.description,
+                                probability: null
+                            },
+                            population_density: {
+                                number: geoAdminResults.download.pop.number,
+                                description: geoAdminResults.download.pop.description,
+                                probability: null
+                            },
+                            community_type: {
+                                id: geoAdminResults.download.type.tag.id,
+                                type: geoAdminResults.download.type.tag.name,
+                                community_id: geoAdminResults.download.type.res.comId,
+                                community_name: geoAdminResults.download.type.res.comName,
+                                canton_id: geoAdminResults.download.type.res.canId,
+                                canton_name: geoAdminResults.download.type.res.canName,
+                                description: geoAdminResults.download.type.tag.description,
+                                probability: null
+                            }
                         },
-                        population_density: {
-                            number: null,
-                            description: null,
-                            probability: null
-                        },
-                        community_type: {
-                            id: null,
-                            type: null,
-                            community_id: null,
-                            postal_code: null,
-                            community_name: null,
-                            canton_id: null,
-                            canton_name: null,
-                            description: null,
-                            probability: null
-                        }
-                    },
-                    upload: {
-                        geographical_surroundings: {
-                            id: geographicalSurroundingsResult.upload.tag.id,
-                            name: geographicalSurroundingsResult.upload.tag.name,
-                            osm_tag: geographicalSurroundingsResult.upload.osm_tag,
-                            description: geographicalSurroundingsResult.upload.tag.description,
-                            probability: null
-                        },
-                        population_density: {
-                            number: null,
-                            description: null,
-                            probability: null
-                        },
-                        community_type: {
-                            id: null,
-                            type: null,
-                            community_id: null,
-                            postal_code: null,
-                            community_name: null,
-                            canton_id: null,
-                            canton_name: null,
-                            description: null,
-                            probability: null
+                        upload: {
+                            geographical_surroundings: {
+                                id: geographicalSurroundingsResult.upload.geo.id,
+                                name: geographicalSurroundingsResult.upload.geo.name,
+                                osm_tag: geographicalSurroundingsResult.upload.osm_tag,
+                                description: geographicalSurroundingsResult.upload.geo.description,
+                                probability: null
+                            },
+                            population_density: {
+                                number: geoAdminResults.upload.pop.number,
+                                description: geoAdminResults.upload.pop.description,
+                                probability: null
+                            },
+                            community_type: {
+                                id: geoAdminResults.upload.type.tag.id,
+                                type: geoAdminResults.upload.type.tag.name,
+                                community_id: geoAdminResults.upload.type.res.comId,
+                                community_name: geoAdminResults.upload.type.res.comName,
+                                canton_id: geoAdminResults.upload.type.res.canId,
+                                canton_name: geoAdminResults.upload.type.res.canName,
+                                description: geoAdminResults.upload.type.tag.description,
+                                probability: null
+                            }
                         }
                     }
-                }
-            });
+                });
 
-            res.end(json);
-        }
-    );
+                res.end(json);
+            }
+        );
+    }
 }
 
 
@@ -92,8 +105,12 @@ function filterPositions(positions) {
 
     positions.forEach(function (element) {
         var lowerCase = element.phase.toLowerCase();
-        if(lowerCase === 'downloadstart' || lowerCase === 'downloadend' || lowerCase === 'uploadend') {
-            newPositions.push(element);
+        if(lowerCase === 'downloadstart') {
+            newPositions[0] = element;
+        } else if(lowerCase === 'downloadend') {
+            newPositions[1] = element;
+        } else if(lowerCase === 'uploadend') {
+            newPositions[2] = element;
         }
     });
 
