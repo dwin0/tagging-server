a4d92b4b06cb439a6fd1e70057a762078b592fe5
dwin94 <dwindler@hsr.ch>
2017-05-26 21:04:40 +0200

    Fluider Header

--- public/javascripts/materialDesign.js
+++ public/javascripts/materialDesign.js
@@ -1,4 +1,13 @@
 //Collapsing Elements
 $(document).ready(function(){
     $('.collapsible').collapsible();
+
+
+    $('.button-collapse').sideNav({
+            menuWidth: 400,
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true // Choose whether you can drag to open on touch screens
+        }
+    );
 });
