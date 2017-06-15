3cc7155014cb5015c79a4d179b2c927915d5c104
aha93 <a1hauser@hsr.ch>
2017-06-15 20:03:47 +0200

    Codedokumentation surroundingsCommunication

--- business_logic/v5.1/surroundingsCommunication.js
+++ business_logic/v5.1/surroundingsCommunication.js
@@ -1,3 +1,9 @@
+/**
+ * This module chooses the best positions with help of the positionsHelper, passes them on to geographicalSurroundings and
+ * populationSurroundings and prepares the answer of those delegations with help of the jsonHelper for the answer to the client.
+ * @module business_logic/surroundingsCommunication
+ */
+
 var populationSurroundings = require('./populationSurroundings');
 var geographicalSurroundings = require('./geographicalSurroundings');
 var parallel = require('async/parallel');
@@ -6,6 +12,12 @@ var positionsHelper = require('./positionsHelper');
 var logError = require('./errorLogger').logError;
 
 
+/**
+ * Function which filters the best positions and returns the result of the surroundings calculation.
+ *
+ * @param {object} req - client request
+ * @param {object} res - response which will be sent to the client
+ */
 function getSurroundings(req, res) {
 
     positionsHelper.choosePositions(req.body, res, function (positions) {
@@ -19,6 +31,16 @@ function getSurroundings(req, res) {
     });
 }
 
+
+/**
+ * Function which starts the calculation of the geographical surroundings as well as the population density and the community type.
+ *
+ * @param {Array} positions - the array is expected to have the best three positions in it, positions should be the result of choosePositions from positionsHelper
+ * @see {business_logic/positionsHelper}
+ * @param {object} body - part of the request, will only be used in case of an error, so that the body which triggers the error can be logged
+ * @param {object} res - response object, will be used to send an error if something went wrong otherwise the result of the
+ * geographical surroundings calculation is returned.
+ */
 function calculateSurroundings(positions, body, res) {
 
     parallel([
